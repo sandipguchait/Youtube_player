43994d94944d5aed687ad179ad9eba53e1d663b9
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-04 21:25:30 +0530

    configuring video select

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,8 @@ import youtube from "../components/apis/youtube";
 
 class App extends React.Component {
 	state = {
-		videos: []
+		videos: [],
+		selectedVideo: null
 	};
 	onTermSubmit = async term => {
 		const response = await youtube.get("/search", {
@@ -16,11 +17,18 @@ class App extends React.Component {
 		this.setState({ videos: response.data.items });
 	};
 
+	onVideoSelect = video => {
+		this.setState({ selectedVideo: video });
+	};
+
 	render() {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
-				<VideoList videos={this.state.videos} />
+				<VideoList
+					onVideoSelect={this.onVideoSelect}
+					videos={this.state.videos}
+				/>
 			</div>
 		);
 	}
