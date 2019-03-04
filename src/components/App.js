56588c99abdabd526d025f649b3534388b0b4ea7
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-04 20:01:20 +0530

    fetching the data

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,8 @@ import SearchBar from "./SearchBar";
 import youtube from "../components/apis/youtube";
 
 class App extends React.Component {
-	onTermSubmit = term => {
-		youtube.get("/search", {
+	onTermSubmit = async term => {
+		const response = await youtube.get("/search", {
 			params: {
 				q: term
 			}
