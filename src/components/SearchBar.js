30f67a25c1aa72da3eb41311200b9da62df74990
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-04 19:37:19 +0530

    handling form and inputs

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -5,13 +5,24 @@ class SearchBar extends React.Component {
 		term: ""
 	};
 
+	onInputChange = event => {
+		this.setState({ term: event.target.value });
+	};
+	onFormSubmit = event => {
+		event.preventDefault();
+	};
+
 	render() {
 		return (
 			<div className="search-bar ui segment">
-				<form className="ui form">
+				<form className="ui form" onSubmit={this.onFormSubmit}>
 					<div className="field">
 						<label>Video Search</label>
-						<input type="text" value={this.state.term} />
+						<input
+							type="text"
+							value={this.state.term}
+							onChange={this.onInputChange}
+						/>
 					</div>
 				</form>
 			</div>
