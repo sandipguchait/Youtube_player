1e3dc4a16b32a096e7575a4e666f61e2907e111e
Sandip Guchait <35764971+sandipguchait@users.noreply.github.com>
2019-03-07 23:29:54 +0530

    setting components

--- src/components/App.js
+++ src/components/App.js
@@ -34,7 +34,7 @@ class App extends React.Component {
 		return (
 			<div className="ui container">
 				<SearchBar onFormSubmit={this.onTermSubmit} />
-				<div className="ui grid">
+				<div className="ui grid" style={{ marginTop: "10px" }}>
 					<div className="ui row">
 						<div className="eleven wide column">
 							<VideoDetail video={this.state.selectedVideo} />
